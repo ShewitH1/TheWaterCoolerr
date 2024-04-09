58e107f028556b8dd95070781d52f8783c079871
Coy <coySimon01@outlook.com>
2024-04-08 20:33:49 -0400

    Added basic app.gets to render the html pages and added the links to the header nav bar

--- app.py
+++ app.py
@@ -6,3 +6,15 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.get('/templates/job_search.html')
+def job_search():
+    return render_template('job_search.html')
+
+@app.get('/templates/job_listing.html')
+def job_listing():
+    return render_template('job_listing.html')
+
+@app.get('/templates/landing.html')
+def landing():
+    return render_template('landing.html')
